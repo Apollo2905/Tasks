6b7c7cc1c111c42c7eb5c857804f4674ee499960
Apollo2905 <pshashilo@k-tech.ru>
2024-10-03 18:11:57 +0300

    Implementation of logic

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-from typing import Optional, Annotated
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-from fastapi import Depends, FastAPI
-from pydantic import BaseModel
-
-app = FastAPI()
-
-
-class STaskAdd(BaseModel):
-    name: str
-    description: Optional[str] = None
+from database import create_tables, delete_tables
+from router import router as tasks_router
 
 
-class STask(STaskAdd):
-    id: int
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова к работе")
+    yield
+    print("Выключение")
 
 
-tasks = []
-
-
-@app.post("/tasks")
-async def add_task(
-        task: Annotated[STaskAdd, Depends()],
-
-):
-    tasks.append(task)
-    return {"ok": True}
-
-
-# @app.get("/tasks")
-# def get_tasks():
-#     task = Task(name="Запиши это видео")
-#     return {"data": task}
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
