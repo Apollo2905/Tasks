7a1a03c5e1a9b7104c2104453acc8b5cd4b36996
Apollo2905 <pshashilo@k-tech.ru>
2024-10-03 12:00:36 +0300

    Starting commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Optional, Annotated
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+
+
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()],
+
+):
+    tasks.append(task)
+    return {"ok": True}
+
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = Task(name="Запиши это видео")
+#     return {"data": task}
